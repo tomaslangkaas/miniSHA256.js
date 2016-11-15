cc2689609534447e9645702aa1ca5784937c4a4c
tomaslangkaas <tomas.langkaas@gmail.com>
2016-11-15 19:39:08 +0100

    added hmac

---
+++ src/miniSHA256HMAC.js
@@ -0,0 +1,19 @@
+var miniSHA256HMAC = (function(SHA256) {
+    return function(key, keybits) {
+        var inner = [],
+            outer = [],
+            i,
+            hi,
+            ho;
+        if (keybits > 512) key = SHA256().digest(key, keybits);
+        for (i = 0; i < 16; i++) {
+            inner[i] = key[i] ^ 0x36363636;
+            outer[i] = key[i] ^ 0x5c5c5c5c;
+        }
+        hi = SHA256().update(inner, 1);
+        ho = SHA256().update(outer, 1);
+        return function(message, messagebits) {
+            return ho.digest(hi.digest(message, messagebits), 256);
+        }
+    }
+})(miniSHA256);
